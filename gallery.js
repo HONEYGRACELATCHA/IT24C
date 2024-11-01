6f3607a791b6f4a08bb4c8260b78ea03720cdcb6
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-11-02 02:11:41 +1100

    changes to index.html

---
+++ gallery.js
@@ -0,0 +1,20 @@
+class AppletCard {
+    constructor(title, description, link) {
+        this.title = title;
+        this.description = description;
+        this.link = link;
+    }
+
+    createCard() {
+        const cardDiv = document.createElement('div');
+        cardDiv.className = 'card applet-card';
+        cardDiv.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${this.title}</h5>
+                <p class="card-text">${this.description}</p>
+                <a href="${this.link}" class="btn btn-primary applet-btn" style="">Go to Applet</a>
+            </div>
+        `;
+        return cardDiv;
+    }
+}
